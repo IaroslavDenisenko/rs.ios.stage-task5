5db22de78f66cf708f53b6d24c141247eef34a5b
Iaroslav Denisenko <yaroslav1985@gmail.com>
2021-06-14 22:48:59 +0300

    feat: task5 is done

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -3,6 +3,21 @@ import Foundation
 class StockMaximize {
 
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        var restPrices = prices
+        var buyPrices = [Int]()
+        let maxValue = prices.max()
+        var sum = 0
+        for price in prices {
+            restPrices.removeFirst()
+            if price == maxValue! {
+                for buyPrice in buyPrices {
+                    sum += price - buyPrice
+                }
+                sum += countProfit(prices: restPrices)
+                break
+            }
+            buyPrices.append(price)
+        }
+        return sum
     }
 }
--- Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -3,6 +3,47 @@ import UIKit
 class MessageDecryptor: NSObject {
     
     func decryptMessage(_ message: String) -> String {
-        return ""
+        var result = message
+        let pat = "\\d*\\[[a-z]*\\]"
+        let regex = NSRegularExpression(pat)
+        let range = NSRange(location: 0, length: message.utf16.count)
+        let matches = regex.matches(in: message, options: [], range: range)
+        if matches.isEmpty { return result }
+        for match in matches {
+            if let matchRange = Range(match.range, in: message) {
+                let encodingMessage = message[matchRange]
+                let startIndex = encodingMessage.startIndex
+                let endIndex = encodingMessage.endIndex
+                var currentIndex = startIndex
+                var counter = ""
+                for i in 0..<encodingMessage.count {
+                    currentIndex = encodingMessage.index(startIndex, offsetBy: i)
+                    let symbol = String(encodingMessage[currentIndex])
+                    if Int(symbol) != nil {
+                        counter += symbol
+                    } else {
+                        break
+                    }
+                }
+                let startWordIndex = encodingMessage.index(currentIndex, offsetBy: 1)
+                let endWordIndex = encodingMessage.index(before: endIndex)
+                let wordRange = startWordIndex..<endWordIndex
+                let word = String(encodingMessage[wordRange])
+                
+                result = regex.stringByReplacingMatches(in: message, options: [], range: match.range, withTemplate: String(repeating: word, count: Int(counter) ?? 1))
+            }
+        }
+        
+        return decryptMessage(result)
+    }
+}
+
+extension NSRegularExpression {
+    convenience init(_ pattern: String) {
+        do {
+            try self.init(pattern: pattern)
+        } catch {
+            preconditionFailure("Illegal regular expression: \(pattern).")
+        }
     }
 }
